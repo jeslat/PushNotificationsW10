1e5310e506920fdf0e6133d10aac3b0bc7952b57
Jesús Latorre <jla@tid.es>
2015-05-08 09:42:43 +0200

    Add send uri by email and enable deploy in a windows phone device

--- AppPushNotifications/AppPushNotifications/MainPage.xaml.cs
+++ AppPushNotifications/AppPushNotifications/MainPage.xaml.cs
@@ -102,5 +102,11 @@ namespace AppPushNotifications
             }
             return false;
         }
+
+        private async void SendUriByEmail_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var mailto = new Uri("mailto:?to=email@example.com&subject=Push notifications uri&body=" + TextBoxUri.Text);
+            await Windows.System.Launcher.LaunchUriAsync(mailto);
+        }
     }
 }
